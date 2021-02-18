b1f408a7f72c1e6db4775df765c77d82e17be74e
wonAdam <ehdgus5500@gmail.com>
2021-02-18 13:17:10 +0900

    ContinueButton Added

---
+++ Assets/ContinueButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinueButton : MonoBehaviour
+{
+    [SerializeField] Transform pos;
+    [SerializeField] int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
