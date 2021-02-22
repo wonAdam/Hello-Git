dad7bbef0595f1512d7f86c2f73024a95610e339
Wondong5500 <ehdgus5500@naver.com>
2021-02-22 17:27:26 +0900

    PopupPanel

---
+++ Assets/PopupPanel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupPanel : MonoBehaviour
+{
+    [SerializeField] int btnCnt;
+
+    public void Unset()
+    {
+        gameObject.SetActive(false);
+    }
+}
